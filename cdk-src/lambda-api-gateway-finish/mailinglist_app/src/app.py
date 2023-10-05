25bc1b704000fd00b6a32d117397bd597e45a1f5
Efi Merdler-Kravitz <efi.merdler@gmail.com>
2023-10-05 16:36:00 +0000

    wip

---
+++ cdk-src/lambda-api-gateway-finish/mailinglist_app/src/app.py
@@ -0,0 +1,28 @@
+import json
+import os
+import boto3
+from boto3.dynamodb.conditions import Key
+
+
+
+# Cache client
+session = boto3.Session()
+dynamodb = session.resource("dynamodb")
+SUBSCRIBERS_TABLE = os.environ.get("SUBSCRIBERS_TABLE")
+table = dynamodb.Table(SUBSCRIBERS_TABLE)
+def lambda_handler(event, context):
+    # Get group name
+    group = event.get("pathParameters", {}).get("group")
+    if group:
+        response = table.query(
+            KeyConditionExpression=Key('group_name').eq(group)
+        )
+        return {
+            "statusCode": 200,
+            "body": json.dumps(response['Items']),
+        }
+    else:
+        return {
+            "statusCode": 500,
+            "body": "Missing group!",
+        }
